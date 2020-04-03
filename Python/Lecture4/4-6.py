26407277cfc915e586380f546412bec1b047f495
hamin <ggamini7@gmail.com>
2020-04-04 00:13:10 +0900

    upload lecture4 code

---
+++ Python/Lecture4/4-6.py
@@ -0,0 +1,5 @@
+print('what a wonderful world')
+number = 1
+while number <= 5:
+    print('python good')
+    number=number+1
---
+++ Python/Lecture4/4-7.py
@@ -0,0 +1,18 @@
+number = 1
+while number <= 10:
+    print('num'+str(number))
+    number=number+1
+
+print('case1')
+number=1
+while number <= 5:
+    print('fun')
+    number=number+1
+    print('study hard')
+
+print()
+print('case2')
+while number <= 5:
+    print('fun')
+    number=number+1
+print('study hard') #들여쓰기 여부 차이 보기
