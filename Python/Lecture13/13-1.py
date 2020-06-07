c350d3fec5deff15391802c785c02386927ce47b
hamin <ggamini7@gmail.com>
2020-06-07 22:44:45 +0900

    upload

---
+++ Python/Lecture13/13-1.py
@@ -0,0 +1,16 @@
+import random
+
+n = int(input('투표자 수:'))
+m = int(input('후보자 수:'))
+f = []
+
+for i in range(m):
+    f.append(0)
+
+for i in range(n):
+    tp = random.randint(1,3)
+    f[tp-1] = f[tp-1] + 1
+
+for i in range(m):
+    print(i+1,'번쨰 후보의 투표 빈도:', f[i])
+    
